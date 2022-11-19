f50361a8741194985ecc1854bce10c4c8b684026
LifThiago <lifthiago@gmail.com>
2022-11-19 12:43:34 -0300

    working on landing

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './Landing.css'
+// import './Landing.css'
+import './Otro.css'
 import venusaurGrande from '../../Images/venusaurGrande.png'
 
 export default function Landing() {
   return (
   <div className='landing'>
     <div className="landing_div">
+      {/* <div className="landing_text"> */}
       <h1 className='landing_welcome' >Welcome to my Pokédex</h1>
-      <Link to='/home' className='landing_link'>GOTTA CATCH 'EM ALL</Link>
+      <Link to='/home' className='landing_link'>Gotta catch 'em all</Link>
+      {/* </div> */}
+      {/* <Link to='/home' className='landing_link'>GOTTA CATCH 'EM ALL</Link> */}
     </div>
   </div>
   )
