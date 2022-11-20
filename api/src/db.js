0c96b2d7d159ad96cf1664e1ba2610dcee1380dd
LifThiago <lifthiago@gmail.com>
2022-11-20 19:47:29 -0300

    added env.CI=false

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL
+  DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL, CI
 } = process.env;
 
 /* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
